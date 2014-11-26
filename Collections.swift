6e6174672c7c58739bdeedb675226f1eed1e202a
airspeedswift <ben@airspeedvelocity.net>
2014-11-26 16:17:48 -0500

    Update Collections.swift

--- Collections.swift
+++ Collections.swift
@@ -11,6 +11,37 @@ func mapSome<S: SequenceType, C: ExtensibleCollectionType>(source: S, transform:
     return result
 }
 
+/// Return an collection containing the results of mapping `transform`
+/// over `source`, when transform does not return nil.
 func mapSome<S: SequenceType, T>(source: S, transform: S.Generator.Element -> T?) -> [T] {
     return mapSome(source, transform)
+}
+
+/// Return an collection containing the results of mapping `combine`
+/// over each element of `source`, carrying the result forward to combine
+/// with the next element.  `initial` becomes the first element of the result.
+///
+/// e.g. `combine([1,2,3],0,+)` returns `[0,1,3,6]`
+func accumulate
+  <S: SequenceType, C: ExtensibleCollectionType>
+  (source: S, var initial: C.Generator.Element, combine: (C.Generator.Element, S.Generator.Element) -> C.Generator.Element)
+  -> C {
+    var result = C()
+    result.append(initial)
+    for x in source {
+        initial = combine(initial, x)
+        result.append(initial)
+    }
+    return result
+}
+
+/// Return an collection containing the results of mapping `combine`
+/// over each element of `source`, carrying the result forward to combine
+/// with the next element.  `initial` becomes the first element of the result.
+///
+/// e.g. `combine([1,2,3],0,+)` returns `[0,1,3,6]`
+func accumulate<S: SequenceType, U>
+  (source: S, initial: U, combine: (U, S.Generator.Element)->U)
+  -> [U] {
+    return accumulate(source, initial, combine)
 }
