7e509395be8a8d130e3ec87067dca9476fe6a9e0
Airspeed Velocity <ben@airspeedvelocity.net>
2014-11-27 15:21:24 -0500

    Restore iOS build

---
+++ SwooshKit-iOS/SwooshKit-iOS.h
@@ -0,0 +1,19 @@
+//
+//  SwooshKit-iOS.h
+//  SwooshKit-iOS
+//
+//  Created by Ben Cohen on 11/27/14.
+//  Copyright (c) 2014 Airspeed Velocity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwooshKit-iOS.
+FOUNDATION_EXPORT double SwooshKit_iOSVersionNumber;
+
+//! Project version string for SwooshKit-iOS.
+FOUNDATION_EXPORT const unsigned char SwooshKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwooshKit_iOS/PublicHeader.h>
+
+
